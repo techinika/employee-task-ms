8d87f4440cb438430b342ea32d3df5536df02c20
Aime Bruce <iraguhaaimebruce@gmail.com>
2023-03-28 23:35:11 +0200

    Responsiveness if Employee pages

--- js/emp_home.js
+++ js/emp_home.js
@@ -5,10 +5,13 @@
 // New codes
 var unfinished_div = document.querySelector(".unfinished_div");
 var fin_div = document.querySelector(".fin_div");
+var every_step = document.querySelector(".every_step");
 
-setInterval(()=>{
-    var w = window.innerWidth;
+// setInterval(()=>{
+//     var w = window.innerWidth;
 
-    unfinished_div.style.width = w;
-    fin_div.style.width = w;
-},100);
+//     unfinished_div.style.width = w;
+//     fin_div.style.width = w;
+//     every_step.style.width = w;
+
+// },100);
